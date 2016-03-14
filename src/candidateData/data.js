89063af86ca81d5ba1b126c0bc1b6c0f8edf3ecc
Frank Moreno <frankmoreno1993@gmail.com>
2016-03-14 16:52:27 -0500

    Info candidatos: 3 primeros

---
+++ src/candidateData/data.js
@@ -0,0 +1,38 @@
+// base contiene la informacion base de los conditados
+//  id: numero de identificacion
+//  nom: nombres
+//  ape: Apellidos
+//  org: organizacion
+//  st: status (activo, tachado, renuncio)
+var base = [
+  {
+    id: 1,
+    nom: 'ALAN GABRIEL LUDWIG',
+    ape: 'GARCIA PEREZ',
+    age: 66,
+    prof: 'ABOGADO'
+    prov: 'LIMA'
+    org: 'ALIANZA POPULAR',
+    st: 'activo'
+  },
+  {
+    id: 2,
+    nom: 'ALEJANDRO',
+    ape: 'TOLEDO MANRRIQUE',
+    age: 70,
+    prof: 'ECONOMISTA'
+    prov: 'ANCASH'
+    org: 'PERU POSIBLE',
+    st: 'activo'
+  },
+  {
+    id: 3,
+    nom: 'ANTERO',
+    ape: 'FLORES ARAOZ ESPARZA',
+    age: 73,
+    prof: 'LIMA'
+    prov: 'ANCASH'
+    org: 'PARTIDO POLITICO  ORDEN',
+    st: 'activo'
+  },
+];
