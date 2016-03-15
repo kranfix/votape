51b011c4542e48e44cca716ce959d3d4a867e4ef
wilsanph <wilsanph@gmail.com>
2016-03-14 23:47:12 -0500

    -> Updated the strings.js file to use the demoText with no wordWrap

--- src/config/strings.js
+++ src/config/strings.js
@@ -6,8 +6,8 @@ window['strings'] =
 								 'fill' : '0xff0000', 'wordWrap' : true, 'wordWrapWidth' : 100,
 								'text' : 'ADVERTENCIA' },
 	'STR_PANEL1_SCENE_INFO' : { 'size' : 20, 'font' : 'Arial', 'align' : 'left', 
-								'fill' : '0x000000', 'wordWrap' : true, 'wordWrapWidth' : 100,
-								'text' : 'No apto para votantes desinformados. Visita http://www.jne.gob.pe/' },
+								'fill' : '0x000000', 'wordWrap' : false, 'wordWrapWidth' : 100,
+								'text' : 'No apto para votantes desinformados.\n\r Visita http://www.jne.gob.pe/' },
 	'STR_TEAM_NAME' : { 'size' : 30, 'font' : 'Arial', 'align' : 'left', 
 						'fill' : '0xffffff', 'wordWrap' : true, 'wordWrapWidth' : 100,
 						'text' : 'Democracia Team presenta' }
